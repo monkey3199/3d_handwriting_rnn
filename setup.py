6ec9044335575a0b2fd233cc81abb1fef656ed54
monkey3199 <monkey3199@gmail.com>
2019-02-11 21:45:31 +0900

    setup 파일 추가

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='3d_handwriting'
+    install_requires=[
+        'tensorflow-gpu>=1.12.0',
+        'matplotlib',
+        'pandas',
+        'scikit-learn',
+        'numpy'
+    ]
+)
